233522f8962b389b35d4a9a07f72e2b3c2820da3
David Hewitt <davidmhewitt@gmail.com>
2020-05-31 12:12:25 +0100

    Try adding flatpak repo after bus start

--- tests/test-runner.sh
+++ tests/test-runner.sh
@@ -7,4 +7,7 @@ ln -s /var/run /run
 dbus-daemon --system --fork
 /usr/lib/policykit-1/polkitd --no-debug &
 /usr/lib/packagekit/packagekitd --backend aptcc &
+
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 $1
